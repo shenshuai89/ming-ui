782748fe58ed29f1659c0c263f812fc36940ca66
申帅 <shuai.shen@ersoft.cn>
2022-05-06 16:14:45 +0800

    feat: 安装依赖，并添加babel对ts进行解析

---
+++ babel.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  presets: [
+    "@babel/preset-env",
+    "@babel/preset-typescript", // 解析ts语法，在采用preset-env
+  ],
+  overrides: [
+    {
+      test: /\.vue$/,
+      plugins: [
+        "@babel/transform-typescript",
+      ],
+    },
+  ],
+  env: {
+    utils: {
+      plugins: [
+        [
+          "babel-plugin-module-resolver", // 为了能正确找到ming-ui模块
+          { root: "ming-ui" },
+        ],
+      ],
+    },
+  },
+};
