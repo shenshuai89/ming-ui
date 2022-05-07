53cc5f2c93331e70c37411177a207756fd5beca9
申帅 <shuai.shen@ersoft.cn>
2022-05-07 22:59:09 +0800

    feat: col的响应式布局设置

--- examples/App.vue
+++ examples/App.vue
@@ -13,8 +13,8 @@
       <m-col :span="8"><div style="background: purple">333</div></m-col>
     </m-row>
      <m-row justify="space-around">
-      <m-col :span="2"><div style="background: red">111</div></m-col>
-      <m-col :span="2"><div style="background: pink">222</div></m-col>
+      <m-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1"><div style="background: red">111</div></m-col>
+      <m-col :span=34><div style="background: pink">222</div></m-col>
       <m-col :span="2"><div style="background: purple">333</div></m-col>
     </m-row>
   </div>
--- packages/col/src/col.ts
+++ packages/col/src/col.ts
@@ -14,36 +14,69 @@ export default defineComponent({
       type: Number,
       default: 0,
     },
+    xs: {
+      type: Number,
+      default: 0,
+    },
+    sm: {
+      type: Number,
+      default: 0,
+    },
+    md: {
+      type: Number,
+      default: 0,
+    },
+    lg: {
+      type: Number,
+      default: 0,
+    },
+    xl: {
+      type: Number,
+      default: 0,
+    },
   },
   setup(props, { slots }) {
     const gutter = inject("ZRow", 0);
 
-    const styleClass = computed(() => {
+    let styleClass = computed(() => {
       const post = ["span", "offset"] as const;
       const allClass = [];
       post.forEach((item) => {
         const size = props[item];
         if (typeof size === "number" && size > 0) {
-          allClass.push(`m-col-${item}-${props[item]}`);
+          if(size<=24){
+            allClass.push(`m-col-${item}-${props[item]}`);
+          } else{
+            allClass.push(`m-col-${item}-24`);
+          }
         }
       });
+
+      const sizes = ["xs", "sm", "md", "lg", "xl"] as const;
+      sizes.forEach((size) => {
+        if (typeof props[size] === "number" && props[size] > 0) {
+          allClass.push(`m-col-${size}-${props[size]}`);
+        }
+      });
+
       return ["m-col", ...allClass];
     });
 
     const styles = computed(() => {
-        if(gutter){
-            return {
-                paddingLeft: gutter/2 + 'px',
-                paddingRight: gutter/2 + 'px',
-            }
-        }
-    })
+      if (gutter) {
+        return {
+          paddingLeft: gutter / 2 + "px",
+          paddingRight: gutter / 2 + "px",
+        };
+      }
+    });
+
     return () => {
       return h(
         props.tag,
         {
           class: styleClass.value,
-          style: styles.value
+          style: styles.value,
         },
         slots.default?.()
       );
