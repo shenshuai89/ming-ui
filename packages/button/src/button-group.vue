c013327998e7684c8311541a5462792926f29164
申帅 <shuai.shen@ersoft.cn>
2022-05-08 11:17:05 +0800

    feat: 设置button-group组件的type类型

--- packages/button/src/button-group.vue
+++ packages/button/src/button-group.vue
@@ -1,11 +1,39 @@
 <template>
-  <div class="m-button-group">
+  <div :class="styleClass">
     <slot></slot>
   </div>
 </template>
-<script>
-import { defineComponent } from "vue";
+<script lang="ts">
+import { computed, defineComponent, PropType } from "vue";
+import {BtnType} from "./button.vue"
 export default defineComponent({
   name: "MButtonGroup",
+  props:{
+    type: {
+      type: String as PropType<BtnType>,
+      default: "", // 默认type为空，取内部button的类型，如果设置则覆盖 子button中设置的type
+      validator: (val: string) => {
+        return [
+          "primary",
+          "success",
+          "warning",
+          "danger",
+          "default",
+          "info",
+        ].includes(val);
+      },
+    },
+  },
+  setup(props){
+    const styleClass = computed(()=>{
+      return [
+        "m-button-group",
+        props.type ? "m-button-group--" + props.type:"",
+      ];
+    })
+    return {
+      styleClass
+    }
+  }
 });
 </script>
