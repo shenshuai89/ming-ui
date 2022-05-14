1eb39bcec385738e8594b6e4bfc269e8e320f0bb
申帅 <shuai.shen@ersoft.cn>
2022-05-14 20:14:20 +0800

    feat: 调整logo

--- examples/view/HeaderView.vue
+++ examples/view/HeaderView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="header">MingUI</div>
+  <div class="header"><img src="../static/images/logo.png">MingUI</div>
 </template>
 <style lang="scss">
 .header {
@@ -7,10 +7,14 @@
   height: 50px;
   box-sizing: border-box;
   background: #f3f3f3;
-  color: #409eff;
+  color: #0eb28b;
   border-bottom: 1px solid #e3e3e3;
   font-size: 30px;
+  font-weight: bold;
   padding-left: 20px;
   padding-top: 3px;
+  img{
+    width: 40px;
+  }
 }
 </style>
