8836a6e2dc8cf912ef27c6546d1570c6ef0e37ff
申帅 <shuai.shen@ersoft.cn>
2022-05-17 11:23:29 +0800

    fix: 修复icon图标组件

--- packages/icon/src/icon.vue
+++ packages/icon/src/icon.vue
@@ -1,9 +1,15 @@
 <template>
-  <div>test component</div>
+  <i :class="`m-icon-${name}`"></i>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 export default defineComponent({
   name: "MIcon",
+  props: {
+    name: {
+      type: String,
+      default: "",
+    },
+  },
 });
 </script>
